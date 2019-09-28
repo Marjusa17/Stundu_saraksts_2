bc800ed7ba58ed536bd316aeae978cf8dbb62859
Markuss <brikmanisbusiness@gmail.com>
2019-09-28 11:49:35 +0300

    1.0.0.1 version

---
+++ src/com/company/mainActivity.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.swing.*;
+
+public class mainActivity  {
+    private JButton button1;
+    private JPanel panel1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("mainActivity");
+        frame.setContentPane(new mainActivity().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
