f24ac05dda5e78fc8f98b07cdccb10073833c491
Markuss <brikmanisbusiness@gmail.com>
2019-10-04 12:24:32 +0300

    Login v002

--- src/com/company/LoginActivity.java
+++ src/com/company/LoginActivity.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 
 public class LoginActivity {
     private JPanel panel1;
-    private JButton button1;
     private JButton button2;
     private JTextField textField1;
     private JTextField textField2;
