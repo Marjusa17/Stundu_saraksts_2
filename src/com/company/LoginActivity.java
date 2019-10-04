d7089c0b2aff7e2b7127cde2ee53eb3b217ff30f
Reinis <ziverts.reinis@gmail.com>
2019-10-04 12:02:30 +0300

    Jauns

---
+++ src/com/company/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import javax.swing.*;
+
+public class LoginActivity {
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
